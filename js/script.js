083f8a958be2f6bebcee9385beafe85b0c812385
maria <maria@arhamsoft.com>
2022-11-04 02:48:45 -0700

    The Robot landing page first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// $('.cr-one').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:true,
+//     // autoplay:true,
+//     // autoplayTimeout:1500,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:2
+//         },
+//         768:{
+//             items:2
+//         },
+//         991:{
+//             items:2
+//         },
+//         992:{
+//             items:2
+//         }
+//     }
+// });
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:2
+//         }
+//     }
+// })
